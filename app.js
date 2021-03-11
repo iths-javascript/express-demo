111895198a1f867ef59e849cbce4359327498c0d
David Lundholm <david.lundholm@zocom.se>
2021-03-11 15:03:04 +0100

    Add static middleware and dynamic end point

--- app.js
+++ app.js
@@ -4,19 +4,13 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.use( express.static('public') )
+
 app.set("view engine", "ejs")
 
-
-
-app.get('/', (req,res) => {
-  // res.send("<h1>Hello World!</h1>")
-  res.render("index", {message:'Hello!'})
+app.get('/:message', (req,res) => { 
+  res.render("index", {message: req.params.message})
 })
-
-app.get('/about', (req,res) => {
-  res.render("about")
-})
-
 
 app.listen(PORT, function(){
   console.log(`Server started on port ${PORT}`)

c850a5f0100e57bba0180f1d4dfdddcccf6365be
David Lundholm <david.lundholm@zocom.se>
2021-03-11 13:50:58 +0100

    Add express boilerplate

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.set("view engine", "ejs")
+
+
+
+app.get('/', (req,res) => {
+  // res.send("<h1>Hello World!</h1>")
+  res.render("index")
+})
+
+app.get('/about', (req,res) => {
+  res.render("about")
+})
+
+
+app.listen(PORT, function(){
+  console.log(`Server started on port ${PORT}`)
+})

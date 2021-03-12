1511ab03913836a86b7e7326084258e505e53360
David Lundholm <david.lundholm@zocom.se>
2021-03-12 10:49:27 +0100

    Add form submission

--- app.js
+++ app.js
@@ -5,12 +5,40 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use( express.static('public') )
+app.use( express.urlencoded() )
 
 app.set("view engine", "ejs")
 
-app.get('/:message', (req,res) => { 
-  res.render("index", {message: req.params.message})
+
+const albums = [
+  {title: "Grillkorv", artist: "Svenne", numTracks: 4},
+  {title: "Bananpaj", artist: "Olof", numTracks: 40},
+]
+
+
+
+app.get('/', (req,res) => {
+  res.render("index")
 })
+
+app.get('/albums', (req,res) => {
+  res.render("albums", {albums})
+})
+
+app.get('/:message', (req,res) => {
+  if(req.params.message == "grillkorv"){
+    res.render("secret")
+  }else{
+    res.render("index", {message: req.params.message})
+  }
+})
+
+app.post('/albums', (req,res) => {
+  albums.push(req.body)
+  res.redirect('/albums')
+})
+
+
 
 app.listen(PORT, function(){
   console.log(`Server started on port ${PORT}`)
